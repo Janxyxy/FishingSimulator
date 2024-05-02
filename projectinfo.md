05c8f6a1a20730d35258ea9d41b82d7f333d4892
Janxyxy <jankroupa2@gmail.com>
2024-05-02 21:16:27 +0200

    Update

---
+++ projectinfo.md
@@ -0,0 +1,16 @@
+# Fishing Simulator
+
+Fishing simulator.
+
+## Table of Contents
+
+- [Members](#members)
+- [Usage](#usage)
+
+## Members
+Janxyxy a Jirka
+
+## Usage
+Run the game and fish.
+
+
--- server.js
+++ server.js
@@ -1,7 +1,65 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const cors = require("cors");
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+env = require("dotenv").config();
+
+const app = express();
+app.use(cors());
+const port = 3000;
+
+// Middleware
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(bodyParser.json());
+
+// MySQL Connection
+const connection = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL: " + err.stack);
+    return;
+  }
+  console.log("Connected to MySQL as id " + connection.threadId);
+});
+
+// Routes
+
+app.post("/register", (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  if (!username || !email || !password || !password2) {
+    res.status(400).send("Empty fields detected");
+    return;
+  }
+
+  if (password !== password2) {
+    res.status(400).send("Passwords do not match");
+
+    return;
+  }
+  // Check if user already exists
+
+  // Encrypt the password
+
+  // Create a new user in the database
+
+  // Return 201 Created status
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
